2196110ae2dd79809080fe041aa471cd36efa309
Aakash Sasikumar <aakash3697@gmail.com>
2020-09-13 16:33:25 +0530

    Fixed packaging
    Fixed issue where we could not import sopy when installing from PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
@@ -6,13 +6,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='sopy',
-      version='1.2',
+      version='1.3',
       description='A neat way to write SOPs using python',
       url='https://github.com/AakashSasikumar/SOPy',
       author='Aakash Sasikumar and Sourav Johar',
       author_email='aakash.sasikumar@protonmail.com',
       license='MIT',
-      packages=find_packages(),
+      packages=["./"],
       install_requires=[
           "jinja2",
           "beautifulsoup4",
