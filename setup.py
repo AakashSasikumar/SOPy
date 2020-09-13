32381cde0c86763272986fe4e700c0f1d917e76a
Aakash Sasikumar <aakash3697@gmail.com>
2020-09-13 16:46:08 +0530

    Fixed packaging
    Finally fixed the damn packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
@@ -6,13 +6,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='sopy',
-      version='1.3',
+      version='1.4',
       description='A neat way to write SOPs using python',
       url='https://github.com/AakashSasikumar/SOPy',
       author='Aakash Sasikumar and Sourav Johar',
       author_email='aakash.sasikumar@protonmail.com',
       license='MIT',
-      packages=["./"],
+      packages=find_packages(),
       install_requires=[
           "jinja2",
           "beautifulsoup4",
---
+++ sopy/__init__.py
@@ -0,0 +1,1 @@
+from sopy.sopy import *
