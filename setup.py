421a4dd3000eb8e92943ff96c3acca50a8ae5cb0
Aakash Sasikumar <aakash3697@gmail.com>
2020-09-13 16:48:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='sopy',
-      version='1.4',
+      version='1.5',
       description='A neat way to write SOPs using python',
       url='https://github.com/AakashSasikumar/SOPy',
       author='Aakash Sasikumar and Sourav Johar',
